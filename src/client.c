ea1ad9072feceadf30360bfc3196ed505306d37b
Addison-Stavlo <AddieStavlo@gmail.com>
2019-05-17 11:50:04 -0400

    parse url complete

--- src/client.c
+++ src/client.c
@@ -12,7 +12,8 @@
 /**
  * Struct to hold all three pieces of a URL
  */
-typedef struct urlinfo_t {
+typedef struct urlinfo_t
+{
   char *hostname;
   char *port;
   char *path;
@@ -45,9 +46,44 @@ urlinfo_t *parse_url(char *url)
     6. Overwrite the colon with a '\0' so that we are just left with the hostname.
   */
 
-  ///////////////////
-  // IMPLEMENT ME! //
-  ///////////////////
+  // FIND PATH (the stuff after the /)
+  //find the first slash in the URL
+  const char slash = '/';
+  path = strchr(url, slash);
+
+  if (path != NULL)
+  {
+    // Overwrite the slash with a '\0' so that we are no longer considering anything after the slash.
+    path[0] = '\0';
+    // Set the path pointer to 1 character after
+    path += 1;
+    printf("path: %s\n", path);
+  }
+  else
+  {
+    printf("there is no / to find the request path!!!\n");
+  }
+
+  // FIND PORT (the stuff between : and /)
+  //Use strchr to find the first colon in the URL.
+  char colon = ':';
+  port = strchr(url, colon);
+
+  if (port != NULL)
+  {
+    // Overwrite the colon with a '\0' so that we are just left with the hostname.
+    port[0] = '\0';
+    // Set the port pointer to 1 character after the spot returned by strchr.
+    port += 1;
+    printf("port: %s\n", port);
+  }
+  else
+  {
+    printf("there is no : to find the port number!!!\n");
+  }
+
+  // whats left in URL is the hostname
+  printf("hostname: %s\n", url);
 
   return urlinfo;
 }
@@ -76,12 +112,13 @@ int send_request(int fd, char *hostname, char *port, char *path)
 }
 
 int main(int argc, char *argv[])
-{  
-  int sockfd, numbytes;  
+{
+  int sockfd, numbytes;
   char buf[BUFSIZE];
 
-  if (argc != 2) {
-    fprintf(stderr,"usage: client HOSTNAME:PORT/PATH\n");
+  if (argc != 2)
+  {
+    fprintf(stderr, "usage: client HOSTNAME:PORT/PATH\n");
     exit(1);
   }
 
@@ -92,7 +129,7 @@ int main(int argc, char *argv[])
     4. Call `recv` in a loop until there is no more data to receive from the server. Print the received response to stdout.
     5. Clean up any allocated memory and open file descriptors.
   */
-
+  parse_url(argv[1]);
   ///////////////////
   // IMPLEMENT ME! //
   ///////////////////
